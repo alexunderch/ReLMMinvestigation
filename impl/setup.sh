ed2c7b17bc62ce0c0b1428a9addb3b668e0d3ba2
alexunderch <73017461+alexunderch@users.noreply.github.com>
2022-04-11 12:49:03 +0300

    set-ups

---
+++ impl/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+apt update
+apt install software-properties-common
+apt-get install -y git\
+                   libglu1-mesa-dev\
+                   libgl1-mesa-dev\
+                   libosmesa6-dev\
+                   xvfb\
+                   ffmpeg\
+                   curl\
+                   patchelf\
+                   libglfw3\
+                   libglfw3-dev\
+                   cmake\ 
+                   zlib1g\
+                   zlib1g-dev\ 
+                   swig\
+                   wget\
+                   unzip
+
+git clone https://github.com/charlesjsun/ReLMM.git
+git clone https://github.com/alexunderch/ReLMMinvestigation.git
+
+python install_mujoco.py
+
+cd ReLMM
+rm setup.py 
+cp ../ReLMMinvestigation/impl/errata/setup.py setup.py 
+rm softlearning/environments/gym/locobot/urdf/locobot_description.urdf 
+cp ../ReLMMinvestigation/impl/errata/locobot_description.urdf softlearning/environments/gym/locobot/urdf/locobot_description.urdf 
+rm -r others/*
+cp ../ReLMMinvestigation/impl/reproduction/* others/
+
+pip install -e .
+
+
